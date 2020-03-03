d6ba004e5f34429da54e6cbd6733b07d83cd798b
Emery Tate <tateejt@gmail.com>
2020-03-03 02:49:59 +0000

    Automatically backed up by Learn

--- src/ChromeBoisDomain.js
+++ src/ChromeBoisDomain.js
@@ -11,15 +11,17 @@ export default class ChromeBoisDomain extends Component {
   handleKeyDown = (event) => {
     if (event.key === 'a') {
       resize('+')
-    } else if (event.key === 's'){
+    } else if (event.key === 's') {
       resize('-')
     }
   }
-  
+   
   render() {
     return (
       <canvas 
         onMouseMove={this.handleMouseMove}
+        onClick={() => {toggleCycling()}}
+        onKeyDown={this.handleKeyDown}
         width='900'
         height='600'
         tabIndex="0">
